ecd82a5baf5f08549805e20a988ec38c8acdd423
AhmedHamdy <geoAhmedHamdy1@gmail.com>
2022-11-11 17:24:38 +0200

    V2.5

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -8,14 +8,7 @@ eb init udagram-api --platform node.js --region us-east-1
 eb use udagram-api-dev
 
 # TODO: Set Environment Variables" 
-# eb setenv PORT=8080
-# eb setenv POSTGRES_USERNAME=postgres
-# eb setenv POSTGRES_PASSWORD=postgres
-# eb setenv POSTGRES_DB=ameen_db
-# eb setenv POSTGRES_HOST=database-1.cyw0ef0hrchh.us-east-1.rds.amazonaws.com
-# eb setenv POSTGRES_PORT=5432
-
-# eb setenv PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PORT=$POSTGRES_PORT
+eb setenv PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PORT=$POSTGRES_PORT
 
 # TODO: Deploy 
 eb deploy udagram-api-dev
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,5 @@
-aws s3 cp --recursive ./www s3://elasticbeanstalk-us-east-1-669151698173/
-# aws s3 cp --recursive  ./www s3://ameen-udegram/
+# aws s3 cp --recursive ./www s3://elasticbeanstalk-us-east-1-669151698173/
+aws s3 cp --recursive  ./www s3://ameen-udegram/
 
 
 
