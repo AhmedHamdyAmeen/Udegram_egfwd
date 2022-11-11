b2687e9210e2ef33db5475e94ab18c12f956d5b9
AhmedHamdy <geoAhmedHamdy1@gmail.com>
2022-11-11 16:38:18 +0200

    V2.4

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,5 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
-
-
+aws s3 cp --recursive ./www s3://elasticbeanstalk-us-east-1-669151698173/
 # aws s3 cp --recursive  ./www s3://ameen-udegram/
+
+
+
